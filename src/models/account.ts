9d176420d8527b62d9fa8895eebfdb5513c46eb9
cat-hoang <contact@hchoang.me>
2026-07-03 22:50:22 +1000

    implement account debit and credit methods; add transfer service for processing transfers

--- src/models/account.ts
+++ src/models/account.ts
@@ -9,4 +9,19 @@ export class Account {
   get balance(): Cents {
     return this.balanceCents;
   }
+
+  canDebit(amount: Cents): boolean {
+    return this.balanceCents >= amount;
+  }
+
+  debit(amount: Cents): void {
+    if (!this.canDebit(amount)) {
+      throw new Error(`Insufficient funds in ${this.number}`);
+    }
+    this.balanceCents -= amount;
+  }
+
+  credit(amount: Cents): void {
+    this.balanceCents += amount;
+  }
 }
